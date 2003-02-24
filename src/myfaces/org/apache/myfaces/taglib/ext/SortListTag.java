6edaa46714c8ecbfb900a5913b1afcd19d9c3db7
royalts <dev-null@apache.org>
2003-02-24 10:44:21 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@165785 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/myfaces/org/apache/myfaces/taglib/ext/SortListTag.java
@@ -0,0 +1,76 @@
+/**
+ * MyFaces - the free JSF implementation
+ * Copyright (C) 2003  The MyFaces Team (http://myfaces.sourceforge.net)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package net.sourceforge.myfaces.taglib.ext;
+
+import net.sourceforge.myfaces.component.UIPanel;
+import net.sourceforge.myfaces.renderkit.html.ListRenderer;
+import net.sourceforge.myfaces.taglib.MyFacesTag;
+
+import javax.faces.component.UIComponent;
+import javax.faces.webapp.FacesTag;
+
+
+/**
+ * DOCUMENT ME!
+ * @author Thomas Spiegl (latest modification by Author)
+ * @version $Revision$ $Date$
+ */
+public class SortListTag
+    extends MyFacesTag
+{
+    public UIComponent createComponent()
+    {
+        UIPanel panel = new UIPanel();
+        // donot save State
+        panel.setTransient(true);
+        return panel;
+
+    }
+
+    public String getRendererType()
+    {
+        return ListRenderer.TYPE;
+    }
+
+    public void setStyle(String value)
+    {
+        setProperty(UIPanel.CLASS_ATTR, value);
+    }
+
+    public void setColumnClasses(String value)
+    {
+        setProperty(UIPanel.COLUMN_CLASSES_ATTR, value);
+    }
+
+    public void setRowClasses(String value)
+    {
+        setProperty(UIPanel.ROW_CLASSES_ATTR, value);
+    }
+
+    public void setFooterClass(String value)
+    {
+        setProperty(UIPanel.FOOTER_CLASS_ATTR, value);
+    }
+
+    public void setHeaderClass(String value)
+    {
+        setProperty(UIPanel.HEADER_CLASS_ATTR, value);
+    }
+
+}
