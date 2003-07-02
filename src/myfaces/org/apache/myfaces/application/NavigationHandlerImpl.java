8e77c107efe28923bb66d2eee72c49e35a49c394
royalts <dev-null@apache.org>
2003-07-02 14:21:16 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@166040 13f79535-47bb-0310-9956-ffa450edef68

--- src/myfaces/org/apache/myfaces/application/NavigationHandlerImpl.java
+++ src/myfaces/org/apache/myfaces/application/NavigationHandlerImpl.java
@@ -18,21 +18,154 @@
  */
 package net.sourceforge.myfaces.application;
 
+import net.sourceforge.myfaces.MyFacesFactoryFinder;
+import net.sourceforge.myfaces.config.FacesConfig;
+import net.sourceforge.myfaces.config.FacesConfigFactory;
+import net.sourceforge.myfaces.config.NavigationCaseConfig;
+import net.sourceforge.myfaces.config.NavigationRuleConfig;
+
+import javax.faces.FactoryFinder;
 import javax.faces.application.NavigationHandler;
 import javax.faces.context.FacesContext;
+import javax.faces.tree.TreeFactory;
+import javax.servlet.ServletContext;
+import java.util.*;
 
 /**
  * DOCUMENT ME!
- * @author Manfred Geiler (latest modification by $Author$)
+ * @author Thomas Spiegl (latest modification by $Author$)
  * @version $Revision$ $Date$
  */
 public class NavigationHandlerImpl
     extends NavigationHandler
 {
+    private static Map _cazes;
+    private static List _wildcardKeys = new ArrayList();
+
+    private static final String ASTERISK = "*";
+
     public void handleNavigation(FacesContext facesContext,
                                  String actionRef,
                                  String outcome)
     {
-        //TODO
+        String treeId = facesContext.getTree().getTreeId();
+        Map casesMap = getNavigationCases(facesContext);
+
+        String newTreeId = null;
+
+        // Excact match
+        List casesList = (List)casesMap.get(treeId);
+        if (casesList != null)
+        {
+            newTreeId = getTreeId(casesList, actionRef, outcome);
+        }
+
+        if (newTreeId == null)
+        {
+            // Wildcard match
+            List keys = getSortedWildcardKeys();
+            for (int i = 0, size = keys.size(); i < size && newTreeId == null; i++)
+            {
+                String fromTreeId = (String)keys.get(i);
+                if (fromTreeId.length() > 2)
+                {
+                    String prefix = fromTreeId.substring(0, fromTreeId.length() - 2);
+                    if (treeId.startsWith(prefix))
+                    {
+                        casesList = (List)casesMap.get(fromTreeId);
+                        if (casesList != null)
+                        {
+                            newTreeId = getTreeId(casesList, actionRef, outcome);
+                        }
+                    }
+                }
+                else
+                {
+                    casesList = (List)casesMap.get(fromTreeId);
+                    if (casesList != null)
+                    {
+                        newTreeId = getTreeId(casesList, actionRef, outcome);
+                    }
+                }
+            }
+        }
+
+        if (newTreeId != null)
+        {
+            TreeFactory treeFactory = (TreeFactory)FactoryFinder.getFactory(FactoryFinder.TREE_FACTORY);
+            facesContext.setTree(treeFactory.getTree(facesContext, newTreeId));
+        }
+    }
+
+    private String getTreeId(List casesList, String actionRef, String outcome)
+    {
+        for (int i = 0, size = casesList.size(); i < size; i++)
+        {
+            NavigationCaseConfig caze = (NavigationCaseConfig)casesList.get(i);
+            String cazeOutcome = caze.getFromOutcome();
+            String cazeActionRef = caze.getFromActionRef();
+
+            if (!((cazeOutcome != null && !cazeOutcome.equals(outcome)) ||
+                (cazeActionRef != null && !cazeActionRef.equals(actionRef))))
+            {
+                return caze.getToTreeId();
+            }
+        }
+        return null;
+    }
+
+    private List getSortedWildcardKeys()
+    {
+        return _wildcardKeys;
+    }
+
+    private synchronized Map getNavigationCases(FacesContext facesContext)
+    {
+        if (_cazes == null)
+        {
+            ServletContext servletContext = (ServletContext)facesContext.getExternalContext().getContext();
+            FacesConfigFactory fcf = MyFacesFactoryFinder.getFacesConfigFactory(servletContext);
+            FacesConfig fc = fcf.getFacesConfig(servletContext);
+
+            List rules = fc.getNavigationRuleConfigList();
+            int sizei = rules.size();
+            _cazes = new HashMap(sizei);
+
+            for (int i = 0; i < sizei; i++)
+            {
+                NavigationRuleConfig rule = (NavigationRuleConfig)rules.get(i);
+                List cazes = rule.getNavigationCaseConfigList();
+                int sizej = cazes.size();
+
+                String fromTreeId = rule.getFromTreeId();
+                List list = (List)_cazes.get(fromTreeId);
+                if (list == null)
+                {
+                    list = new ArrayList(sizej);
+                    _cazes.put(fromTreeId, list);
+                    if (fromTreeId.endsWith(ASTERISK))
+                    {
+                        _wildcardKeys.add(fromTreeId);
+                    }
+                }
+
+                for (int j = 0; j < sizej; j++)
+                {
+                    NavigationCaseConfig navcase = (NavigationCaseConfig)cazes.get(j);
+                    list.add(navcase);
+                }
+            }
+            Collections.sort(_wildcardKeys, new KeyComparator());
+        }
+        return _cazes;
+    }
+
+    private static class KeyComparator
+        implements Comparator
+    {
+        public int compare(Object o1, Object o2)
+        {
+            return -(((String)o1).compareTo((String)o2));
+        }
     }
 }
