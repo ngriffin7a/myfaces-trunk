e50bf38770435838c64b899a8b29b6cb8a332ed4
dave0000 <dev-null@apache.org>
2003-09-18 00:46:39 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@166147 13f79535-47bb-0310-9956-ffa450edef68

--- src/myfaces/org/apache/myfaces/strict/renderkit/html/GridRenderer.java
+++ src/myfaces/org/apache/myfaces/strict/renderkit/html/GridRenderer.java
@@ -37,7 +37,7 @@ import javax.faces.context.FacesContext;
  * @version $Revision$ $Date$
  */
 public class GridRenderer
-//extends TableRendererSupport
+extends TableRendererSupport
 {
     //~ Static fields/initializers -----------------------------------------------------------------
 
@@ -50,7 +50,6 @@ public class GridRenderer
         return TYPE;
     }
 
-    /*
     protected void renderBody(FacesContext context, UIComponent component, int columns)
     throws IOException
     {
@@ -73,5 +72,4 @@ public class GridRenderer
                 context, children, columns, rowClasses, columnClasses, "td", "tbody", 0);
         }
     }
-    */
 }
