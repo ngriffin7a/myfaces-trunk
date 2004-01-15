961f644afb415cf73f7b71f94e11e1d3fd283f6f
dave0000 <dev-null@apache.org>
2004-01-15 02:38:50 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@166239 13f79535-47bb-0310-9956-ffa450edef68

--- src/myfaces/org/apache/myfaces/config/SupportedComponentClassConfig.java
+++ src/myfaces/org/apache/myfaces/config/SupportedComponentClassConfig.java
@@ -1,8 +1,27 @@
+/*
+ * MyFaces - the free JSF implementation
+ * Copyright (C) 2003  The MyFaces Team (http://myfaces.sourceforge.net)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 package net.sourceforge.myfaces.config;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -15,12 +34,8 @@ public class SupportedComponentClassConfig
 {
     private static final Log log = LogFactory.getLog(SupportedComponentClassConfig.class);
 
+    private final Set _attributeName = new HashSet();
     private Class _componentClass;
-    private Set _attributeName;
-
-    public SupportedComponentClassConfig()
-    {
-    }
 
     public SupportedComponentClassConfig(Class componentClass)
     {
--- src/myfaces/org/apache/myfaces/config/SupportedComponentTypeConfig.java
+++ src/myfaces/org/apache/myfaces/config/SupportedComponentTypeConfig.java
@@ -1,6 +1,24 @@
+/*
+ * MyFaces - the free JSF implementation
+ * Copyright (C) 2003  The MyFaces Team (http://myfaces.sourceforge.net)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 package net.sourceforge.myfaces.config;
 
-import java.util.Map;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -11,8 +29,8 @@ import java.util.Set;
  */
 public class SupportedComponentTypeConfig
 {
+    private final Set _attributeName = new HashSet();
     private String _componentType;
-    private Set _attributeName;
 
     public Set getAttributeNames()
     {
