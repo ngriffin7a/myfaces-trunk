0d4b5997cf283406feea0f42dce522a777915f76
Manfred Geiler <manolito@apache.org>
2004-01-28 09:09:51 +0000

    no message
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@166298 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/myfaces/org/apache/myfaces/renderkit/html/HtmlRenderKitImpl.java
@@ -0,0 +1,10 @@
+package net.sourceforge.myfaces.renderkit.html;
+
+/**
+ * @author Manfred Geiler (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+public class HtmlRenderKitImpl
+{
+    //private static final Log log = LogFactory.getLog(HtmlRenderKitImpl.class);
+}
