55f2b80ae9a8893adb0d59f080702748dfad7ac0
tinytoony <dev-null@apache.org>
2004-01-29 11:30:10 +0000

    new htmlcommandbuttontag
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@166316 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/myfaces/org/apache/myfaces/taglib/html/HtmlCommandButtonTag.java
@@ -0,0 +1,141 @@
+/**
+ * MyFaces - the free JSF implementation
+ * Copyright (C) 2003, 2004  The MyFaces Team (http://myfaces.sourceforge.net)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package net.sourceforge.myfaces.taglib.html;
+
+import net.sourceforge.myfaces.renderkit.JSFAttr;
+import net.sourceforge.myfaces.renderkit.html.HTML;
+
+import javax.faces.component.UIComponent;
+
+
+/**
+ * DOCUMENT ME!
+ * @author Manfred Geiler (latest modification by $Author$)
+ * @author Martin Marinschek
+ * @version $Revision$ $Date$
+ */
+public class HtmlCommandButtonTag
+    extends HtmlComponentTag
+{
+    public String getComponentType()
+    {
+        return "CommandButton";
+    }
+
+    public String getDefaultRendererType()
+    {
+        return "Button";
+    }
+
+    // UIComponent attributes --> already implemented in MyfacesComponentTag
+
+    // user role attributes --> already implemented in MyfacesComponentTag
+
+    // HTML universal attributes --> already implemented in HtmlComponentTag
+
+    // HTML event handler attributes --> already implemented in HtmlComponentTag
+
+    // HTML input attributes relevant for command-button
+    private String _accesskey;
+    private String _alt;
+    private String _disabled;
+    private String _onblur;
+    private String _onchange;
+    private String _onfocus;
+    private String _onselect;
+    private String _size;
+    private String _tabindex;
+
+    // UICommand attributes
+    private String _action;
+    private String _image;
+
+    protected void setProperties(UIComponent component)
+    {
+        super.setProperties(component);
+
+        setStringProperty(component, HTML.ACCESSKEY_ATTR, _accesskey);
+        setStringProperty(component, HTML.ALT_ATTR, _alt);
+        setStringProperty(component, HTML.DISABLED_ATTR, _disabled);
+        setStringProperty(component, HTML.ONBLUR_ATTR, _onblur);
+        setStringProperty(component, HTML.ONCHANGE_ATTR, _onchange);
+        setStringProperty(component, HTML.ONFOCUS_ATTR, _onfocus);
+        setStringProperty(component, HTML.ONSELECT_ATTR, _onselect);
+        setStringProperty(component, HTML.SIZE_ATTR, _size);
+        setStringProperty(component, HTML.TABINDEX_ATTR, _tabindex);
+
+        setStringProperty(component, JSFAttr.ACTION_ATTR, _action);
+        setStringProperty(component, JSFAttr.IMAGE_ATTR, _image);
+   }
+
+    public void setAccesskey(String accesskey)
+    {
+        _accesskey = accesskey;
+    }
+
+    public void setAlt(String alt)
+    {
+        _alt = alt;
+    }
+
+    public void setDisabled(String disabled)
+    {
+        _disabled = disabled;
+    }
+
+    public void setOnblur(String onblur)
+    {
+        _onblur = onblur;
+    }
+
+    public void setOnchange(String onchange)
+    {
+        _onchange = onchange;
+    }
+
+    public void setOnfocus(String onfocus)
+    {
+        _onfocus = onfocus;
+    }
+
+    public void setOnselect(String onselect)
+    {
+        _onselect = onselect;
+    }
+
+    public void setSize(String size)
+    {
+        _size = size;
+    }
+
+    public void setTabindex(String tabindex)
+    {
+        _tabindex = tabindex;
+    }
+
+    public void setAction(String action)
+    {
+        _action = action;
+    }
+
+    public void setImage(String image)
+    {
+        _image = image;
+    }
+}
