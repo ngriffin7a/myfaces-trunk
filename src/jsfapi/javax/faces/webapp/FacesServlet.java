a0948b42abd557a5c355c757cb686df718b81f7a
Manfred Geiler <manolito@apache.org>
2004-03-07 21:30:57 +0000

    no message
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@166408 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/jsfapi/javax/faces/webapp/FacesServlet.java
@@ -0,0 +1,72 @@
+/*
+ * MyFaces - the free JSF implementation
+ * Copyright (C) 2003, 2004  The MyFaces Team (http://myfaces.sourceforge.net)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package javax.faces.webapp;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * DOCUMENT ME!
+ * @author Manfred Geiler (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+public class FacesServlet
+        implements Servlet
+{
+    public static final String CONFIG_FILES_ATTR = "javax.faces.CONFIG_FILES";
+    public static final String LIFECYCLE_ID_ATTR = "javax.faces.LIFECYCLE_ID";
+
+    private static final String SERVLET_INFO = "FacesServlet of the MyFaces API implementation";
+    private ServletConfig _servletConfig;
+
+    public FacesServlet()
+    {
+        super();
+    }
+
+    public void destroy()
+    {
+
+    }
+
+    public ServletConfig getServletConfig()
+    {
+        return _servletConfig;
+    }
+
+    public String getServletInfo()
+    {
+        return SERVLET_INFO;
+    }
+
+    public void init(ServletConfig servletConfig)
+            throws ServletException
+    {
+        _servletConfig = servletConfig;
+        //TODO
+    }
+
+    public void service(ServletRequest request,
+                        ServletResponse response)
+            throws IOException,
+                   ServletException
+    {
+        //TODO
+    }
+}
