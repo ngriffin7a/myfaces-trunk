242923c885a4eb09b5add70075c412d75d893cd9
royalts <dev-null@apache.org>
2004-03-09 16:20:23 +0000

    no message
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@166415 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/jsfapi/javax/faces/el/PropertyResolver.java
@@ -0,0 +1,88 @@
+/**
+ * MyFaces - the free JSF implementation
+ * Copyright (C) 2003, 2004  The MyFaces Team (http://myfaces.sourceforge.net)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package javax.faces.el;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author Thomas Spiegl (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+public abstract class PropertyResolver
+{
+
+	// FIELDS
+
+	// CONSTRUCTORS
+	public PropertyResolver()
+	{
+		//TODO
+		throw new UnsupportedOperationException();
+	}
+
+	// METHODS
+	public Class getType(Object base, int index)
+	{
+        //TODO
+        throw new UnsupportedOperationException();
+	}
+
+	public Class getType(Object base, java.lang.Object property)
+	{
+        //TODO
+        throw new UnsupportedOperationException();
+	}
+
+	public Object getValue(Object base, int index)
+	{
+        //TODO
+        throw new UnsupportedOperationException();
+	}
+
+	public Object getValue(Object base, java.lang.Object property)
+	{
+        //TODO
+        throw new UnsupportedOperationException();
+	}
+
+	public boolean isReadOnly(Object base, int index)
+	{
+        //TODO
+        throw new UnsupportedOperationException();
+	}
+
+	public boolean isReadOnly(Object base, java.lang.Object property)
+	{
+        //TODO
+        throw new UnsupportedOperationException();
+	}
+
+	public void setValue(Object base, int index, java.lang.Object value)
+	{
+        //TODO
+        throw new UnsupportedOperationException();
+	}
+
+	public void setValue(Object base, Object property, java.lang.Object value)
+	{
+        //TODO
+        throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/jsfapi/javax/faces/el/ValueBinding.java
@@ -0,0 +1,72 @@
+/**
+ * MyFaces - the free JSF implementation
+ * Copyright (C) 2003, 2004  The MyFaces Team (http://myfaces.sourceforge.net)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package javax.faces.el;
+
+import javax.faces.context.FacesContext;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author Thomas Spiegl (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+public abstract class ValueBinding
+{
+
+	// FIELDS
+
+	// CONSTRUCTORS
+	public ValueBinding()
+	{
+		//TODO
+		throw new UnsupportedOperationException();
+	}
+
+	// METHODS
+	public String getExpressionString()
+	{
+        //TODO
+        throw new UnsupportedOperationException();
+	}
+
+	public Class getType(FacesContext facesContext)
+	{
+        //TODO
+        throw new UnsupportedOperationException();
+	}
+
+	public Object getValue(FacesContext facesContext)
+	{
+        //TODO
+        throw new UnsupportedOperationException();
+	}
+
+	public boolean isReadOnly(FacesContext facesContext)
+	{
+        //TODO
+        throw new UnsupportedOperationException();
+	}
+
+	public void setValue(FacesContext facesContext, Object value)
+	{
+        //TODO
+        throw new UnsupportedOperationException();
+	}
+
+}
