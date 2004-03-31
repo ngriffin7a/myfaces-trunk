615d0171eff93a986f08076b18dabe5c55dba55a
Manfred Geiler <manolito@apache.org>
2004-03-31 07:19:20 +0000

    changed name from UserRoleSupport
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@166610 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/components/org/apache/myfaces/component/UserRoleAware.java
@@ -0,0 +1,40 @@
+/*
+ * MyFaces - the free JSF implementation
+ * Copyright (C) 2003, 2004  The MyFaces Team (http://myfaces.sourceforge.net)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package net.sourceforge.myfaces.component;
+
+/**
+ * Behavioral interface.
+ * Components that support user role checking should implement this interface
+ * to optimize property access.
+ *
+ * @author Manfred Geiler (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ * $Log$
+ * Revision 1.1  2004/03/31 07:19:20  manolito
+ * changed name from UserRoleSupport
+ *
+ */
+public interface UserRoleAware
+{
+    public String getEnabledOnUserRole();
+    public void setEnabledOnUserRole(String userRole);
+
+    public String getVisibleOnUserRole();
+    public void setVisibleOnUserRole(String userRole);
+}
