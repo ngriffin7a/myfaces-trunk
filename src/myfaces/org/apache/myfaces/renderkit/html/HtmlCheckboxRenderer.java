732395c17115ce18aa91bf35af43462a0e7b1e2c
Manfred Geiler <manolito@apache.org>
2004-04-02 13:37:01 +0000

    logic moved to new base class
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@166642 13f79535-47bb-0310-9956-ffa450edef68

--- src/myfaces/org/apache/myfaces/renderkit/html/HtmlCheckboxRenderer.java
+++ src/myfaces/org/apache/myfaces/renderkit/html/HtmlCheckboxRenderer.java
@@ -18,176 +18,17 @@
  */
 package net.sourceforge.myfaces.renderkit.html;
 
-import net.sourceforge.myfaces.renderkit.JSFAttr;
-import net.sourceforge.myfaces.renderkit.RendererUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
-import javax.faces.FacesException;
-import javax.faces.component.UIComponent;
-import javax.faces.component.UISelectBoolean;
-import javax.faces.component.UISelectMany;
-import javax.faces.component.html.HtmlSelectManyCheckbox;
-import javax.faces.context.FacesContext;
-import javax.faces.context.ResponseWriter;
-import javax.faces.convert.Converter;
-import javax.faces.convert.ConverterException;
-import javax.faces.model.SelectItem;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.Set;
 
 
 /**
  * @author Thomas Spiegl (latest modification by $Author$)
- * @author Thomas Spiegl
  * @author Anton Koinov
  * @version $Revision$ $Date$
  */
 public class HtmlCheckboxRenderer
-        extends HtmlRenderer
+        extends HtmlCheckboxRendererBase
 {
-    private static final Log log = LogFactory.getLog(HtmlCheckboxRenderer.class);
+    //private static final Log log = LogFactory.getLog(HtmlCheckboxRenderer.class);
 
-    private static final String PAGE_DIRECTION = "pageDirection";
-    private static final String LINE_DIRECTION = "lineDirection";
-
-    private static final String EXTERNAL_TRUE_VALUE = "true";
-
-    public void encodeEnd(FacesContext facesContext, UIComponent uiComponent)
-            throws IOException
-    {
-        RendererUtils.checkParamValidity(facesContext, uiComponent, null);
-        if (uiComponent instanceof UISelectBoolean)
-        {
-            HtmlRendererUtils.renderCheckbox(facesContext,
-                                             uiComponent,
-                                             EXTERNAL_TRUE_VALUE,
-                                             null,
-                                             ((UISelectBoolean)uiComponent).isSelected());
-        }
-        else if (uiComponent instanceof UISelectMany)
-        {
-            renderCheckboxList(facesContext, (UISelectMany)uiComponent);
-        }
-        else
-        {
-            throw new IllegalArgumentException("Unsupported component class " + uiComponent.getClass().getName());
-        }
-    }
-
-
-    public void renderCheckboxList(FacesContext facesContext, UISelectMany selectMany)
-            throws IOException
-    {
-        String layout;
-        if (selectMany instanceof HtmlSelectManyCheckbox)
-        {
-            layout = ((HtmlSelectManyCheckbox)selectMany).getLayout();
-        }
-        else
-        {
-            layout = (String)selectMany.getAttributes().get(JSFAttr.LAYOUT_ATTR);
-        }
-        boolean pageDirectionLayout = false; //Default to lineDirection
-        if (layout != null)
-        {
-            if (layout.equals(PAGE_DIRECTION))
-            {
-                pageDirectionLayout = true;
-            }
-            else if (layout.equals(LINE_DIRECTION))
-            {
-                pageDirectionLayout = false;
-            }
-            else
-            {
-                log.error("Wrong layout attribute for component " + selectMany.getClientId(facesContext) + ": " + layout);
-            }
-        }
-
-        ResponseWriter writer = facesContext.getResponseWriter();
-        writer.startElement(HTML.TABLE_ELEM, selectMany);
-        if (!pageDirectionLayout) writer.startElement(HTML.TR_ELEM, selectMany);
-
-        Converter converter;
-        try
-        {
-            converter = RendererUtils.findUISelectManyConverter(facesContext, selectMany);
-        }
-        catch (FacesException e)
-        {
-            log.error("Error finding Converter for component with id " + selectMany.getClientId(facesContext));
-            converter = null;
-        }
-
-        Set lookupSet = RendererUtils.getSelectedValuesAsSet(selectMany);
-
-        for (Iterator it = RendererUtils.getSelectItemList(selectMany).iterator(); it.hasNext(); )
-        {
-            SelectItem selectItem = (SelectItem)it.next();
-            Object itemValue = selectItem.getValue();
-            String itemStrValue;
-            if (converter == null)
-            {
-                itemStrValue = itemValue.toString();
-            }
-            else
-            {
-                itemStrValue = converter.getAsString(facesContext, selectMany, itemValue);
-            }
-
-            writer.write("\t\t");
-            if (pageDirectionLayout) writer.startElement(HTML.TR_ELEM, selectMany);
-            writer.startElement(HTML.TD_ELEM, selectMany);
-            HtmlRendererUtils.renderCheckbox(facesContext,
-                                           selectMany,
-                                           itemStrValue,
-                                           selectItem.getLabel(),
-                                           lookupSet.contains(itemValue));
-            writer.endElement(HTML.TD_ELEM);
-            if (pageDirectionLayout) writer.endElement(HTML.TR_ELEM);
-        }
-
-        if (!pageDirectionLayout) writer.endElement(HTML.TR_ELEM);
-        writer.endElement(HTML.TABLE_ELEM);
-    }
-
-
-    public void decode(FacesContext facesContext, UIComponent uiComponent)
-    {
-        RendererUtils.checkParamValidity(facesContext, uiComponent, null);
-        if (uiComponent instanceof UISelectBoolean)
-        {
-            HtmlRendererUtils.decodeUISelectBoolean(facesContext, uiComponent);
-        }
-        else if (uiComponent instanceof UISelectMany)
-        {
-            HtmlRendererUtils.decodeUISelectMany(facesContext, uiComponent);
-        }
-        else
-        {
-            throw new IllegalArgumentException("Unsupported component class " + uiComponent.getClass().getName());
-        }
-    }
-
-
-    public Object getConvertedValue(FacesContext facesContext, UIComponent uiComponent, Object submittedValue) throws ConverterException
-    {
-        RendererUtils.checkParamValidity(facesContext, uiComponent, null);
-        if (uiComponent instanceof UISelectBoolean)
-        {
-            return submittedValue;
-        }
-        else if (uiComponent instanceof UISelectMany)
-        {
-            return RendererUtils.getConvertedUISelectManyValue(facesContext,
-                                                               (UISelectMany)uiComponent,
-                                                               submittedValue);
-        }
-        else
-        {
-            throw new IllegalArgumentException("Unsupported component class " + uiComponent.getClass().getName());
-        }
-    }
 }
---
+++ src/share/org/apache/myfaces/renderkit/html/HtmlCheckboxRendererBase.java
@@ -0,0 +1,193 @@
+/**
+ * MyFaces - the free JSF implementation
+ * Copyright (C) 2003, 2004  The MyFaces Team (http://myfaces.sourceforge.net)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package net.sourceforge.myfaces.renderkit.html;
+
+import net.sourceforge.myfaces.renderkit.JSFAttr;
+import net.sourceforge.myfaces.renderkit.RendererUtils;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.faces.FacesException;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UISelectBoolean;
+import javax.faces.component.UISelectMany;
+import javax.faces.component.html.HtmlSelectManyCheckbox;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.model.SelectItem;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+
+
+/**
+ * @author Thomas Spiegl (latest modification by $Author$)
+ * @author Anton Koinov
+ * @version $Revision$ $Date$
+ */
+public class HtmlCheckboxRendererBase
+        extends HtmlRenderer
+{
+    private static final Log log = LogFactory.getLog(HtmlCheckboxRendererBase.class);
+
+    private static final String PAGE_DIRECTION = "pageDirection";
+    private static final String LINE_DIRECTION = "lineDirection";
+
+    private static final String EXTERNAL_TRUE_VALUE = "true";
+
+    public void encodeEnd(FacesContext facesContext, UIComponent uiComponent)
+            throws IOException
+    {
+        RendererUtils.checkParamValidity(facesContext, uiComponent, null);
+        if (uiComponent instanceof UISelectBoolean)
+        {
+            HtmlRendererUtils.renderCheckbox(facesContext,
+                                             uiComponent,
+                                             EXTERNAL_TRUE_VALUE,
+                                             null,
+                                             ((UISelectBoolean)uiComponent).isSelected());
+        }
+        else if (uiComponent instanceof UISelectMany)
+        {
+            renderCheckboxList(facesContext, (UISelectMany)uiComponent);
+        }
+        else
+        {
+            throw new IllegalArgumentException("Unsupported component class " + uiComponent.getClass().getName());
+        }
+    }
+
+
+    public void renderCheckboxList(FacesContext facesContext, UISelectMany selectMany)
+            throws IOException
+    {
+        String layout;
+        if (selectMany instanceof HtmlSelectManyCheckbox)
+        {
+            layout = ((HtmlSelectManyCheckbox)selectMany).getLayout();
+        }
+        else
+        {
+            layout = (String)selectMany.getAttributes().get(JSFAttr.LAYOUT_ATTR);
+        }
+        boolean pageDirectionLayout = false; //Default to lineDirection
+        if (layout != null)
+        {
+            if (layout.equals(PAGE_DIRECTION))
+            {
+                pageDirectionLayout = true;
+            }
+            else if (layout.equals(LINE_DIRECTION))
+            {
+                pageDirectionLayout = false;
+            }
+            else
+            {
+                log.error("Wrong layout attribute for component " + selectMany.getClientId(facesContext) + ": " + layout);
+            }
+        }
+
+        ResponseWriter writer = facesContext.getResponseWriter();
+        writer.startElement(HTML.TABLE_ELEM, selectMany);
+        if (!pageDirectionLayout) writer.startElement(HTML.TR_ELEM, selectMany);
+
+        Converter converter;
+        try
+        {
+            converter = RendererUtils.findUISelectManyConverter(facesContext, selectMany);
+        }
+        catch (FacesException e)
+        {
+            log.error("Error finding Converter for component with id " + selectMany.getClientId(facesContext));
+            converter = null;
+        }
+
+        Set lookupSet = RendererUtils.getSelectedValuesAsSet(selectMany);
+
+        for (Iterator it = RendererUtils.getSelectItemList(selectMany).iterator(); it.hasNext(); )
+        {
+            SelectItem selectItem = (SelectItem)it.next();
+            Object itemValue = selectItem.getValue();
+            String itemStrValue;
+            if (converter == null)
+            {
+                itemStrValue = itemValue.toString();
+            }
+            else
+            {
+                itemStrValue = converter.getAsString(facesContext, selectMany, itemValue);
+            }
+
+            writer.write("\t\t");
+            if (pageDirectionLayout) writer.startElement(HTML.TR_ELEM, selectMany);
+            writer.startElement(HTML.TD_ELEM, selectMany);
+            HtmlRendererUtils.renderCheckbox(facesContext,
+                                           selectMany,
+                                           itemStrValue,
+                                           selectItem.getLabel(),
+                                           lookupSet.contains(itemValue));
+            writer.endElement(HTML.TD_ELEM);
+            if (pageDirectionLayout) writer.endElement(HTML.TR_ELEM);
+        }
+
+        if (!pageDirectionLayout) writer.endElement(HTML.TR_ELEM);
+        writer.endElement(HTML.TABLE_ELEM);
+    }
+
+
+    public void decode(FacesContext facesContext, UIComponent uiComponent)
+    {
+        RendererUtils.checkParamValidity(facesContext, uiComponent, null);
+        if (uiComponent instanceof UISelectBoolean)
+        {
+            HtmlRendererUtils.decodeUISelectBoolean(facesContext, uiComponent);
+        }
+        else if (uiComponent instanceof UISelectMany)
+        {
+            HtmlRendererUtils.decodeUISelectMany(facesContext, uiComponent);
+        }
+        else
+        {
+            throw new IllegalArgumentException("Unsupported component class " + uiComponent.getClass().getName());
+        }
+    }
+
+
+    public Object getConvertedValue(FacesContext facesContext, UIComponent uiComponent, Object submittedValue) throws ConverterException
+    {
+        RendererUtils.checkParamValidity(facesContext, uiComponent, null);
+        if (uiComponent instanceof UISelectBoolean)
+        {
+            return submittedValue;
+        }
+        else if (uiComponent instanceof UISelectMany)
+        {
+            return RendererUtils.getConvertedUISelectManyValue(facesContext,
+                                                               (UISelectMany)uiComponent,
+                                                               submittedValue);
+        }
+        else
+        {
+            throw new IllegalArgumentException("Unsupported component class " + uiComponent.getClass().getName());
+        }
+    }
+}
