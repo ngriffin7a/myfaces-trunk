e5b3acd79c41c08729d778b976e332d73b8db52e
Manfred Geiler <manolito@apache.org>
2004-05-18 07:13:32 +0000

    X-checked against specs: no more synchronization needed, allow replacement of renderKit, no excepetion on unknown id
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@166809 13f79535-47bb-0310-9956-ffa450edef68

--- src/myfaces/org/apache/myfaces/renderkit/RenderKitFactoryImpl.java
+++ src/myfaces/org/apache/myfaces/renderkit/RenderKitFactoryImpl.java
@@ -18,6 +18,9 @@
  */
 package net.sourceforge.myfaces.renderkit;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import javax.faces.FacesException;
 import javax.faces.context.FacesContext;
 import javax.faces.render.RenderKit;
@@ -30,42 +33,55 @@ import java.util.Map;
  * RenderKitFactory implementation as defined in Spec. JSF.7.3
  * @author Manfred Geiler (latest modification by $Author$)
  * @version $Revision$ $Date$
+ * $Log$
+ * Revision 1.12  2004/05/18 07:13:32  manolito
+ * X-checked against specs: no more synchronization needed, allow replacement of renderKit, no excepetion on unknown id
+ *
  */
 public class RenderKitFactoryImpl
     extends RenderKitFactory
 {
-    private static final Object _lock = new Object();
+    private static final Log log = LogFactory.getLog(RenderKitFactoryImpl.class);
+
     private static Map _renderkits = new HashMap();
 
     public RenderKitFactoryImpl()
     {
     }
 
-    public void addRenderKit(String id, RenderKit renderkit)
+
+    public void addRenderKit(String renderKitId, RenderKit renderKit)
     {
-        synchronized (_lock)
+        if (renderKitId == null) throw new NullPointerException("renderKitId");
+        if (renderKit == null) throw new NullPointerException("renderKit");
+        if (log.isInfoEnabled())
         {
-            if (_renderkits.get(id) != null)
+            if (_renderkits.containsKey(renderKitId))
             {
-                throw new IllegalArgumentException("Renderkit with id '" + id + "' already exists.");
+                log.info("RenderKit with renderKitId '" + renderKitId + "' was replaced.");
             }
-            _renderkits.put(id, renderkit);
         }
+        _renderkits.put(renderKitId, renderKit);
     }
 
-    public RenderKit getRenderKit(FacesContext facescontext, String renderKitId)
+
+    public RenderKit getRenderKit(FacesContext context, String renderKitId)
             throws FacesException
     {
-        synchronized (_lock)
+        if (renderKitId == null) throw new NullPointerException("renderKitId");
+        RenderKit renderkit = (RenderKit)_renderkits.get(renderKitId);
+        if (renderkit == null)
         {
-            RenderKit renderkit = (RenderKit)_renderkits.get(renderKitId);
-            if (renderkit == null)
-            {
-                throw new IllegalArgumentException("Unknown renderkit '" + renderKitId + "'.");
-            }
-            return renderkit;
+            //throw new IllegalArgumentException("Unknown RenderKit '" + renderKitId + "'.");
+            //JSF Spec API Doc says:
+            // "If there is no registered RenderKit for the specified identifier, return null"
+            // vs "IllegalArgumentException - if no RenderKit instance can be returned for the specified identifier"
+            //First sentence is more precise, so we just log a warning
+            log.warn("Unknown RenderKit '" + renderKitId + "'.");
         }
+        return renderkit;
     }
+
 
     public Iterator getRenderKitIds()
     {
