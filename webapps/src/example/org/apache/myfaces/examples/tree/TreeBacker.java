d58514dca9f7591d268a0c3a5adefc322df95112
Sean Schofield <schof@apache.org>
2005-04-27 16:17:30 +0000

    Fixed an error in TreeBacker (wasn't causing a problem but information was misleading); Also removed unused method
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@167816 13f79535-47bb-0310-9956-ffa450edef68

--- webapps/src/example/org/apache/myfaces/examples/tree/TreeBacker.java
+++ webapps/src/example/org/apache/myfaces/examples/tree/TreeBacker.java
@@ -28,138 +28,69 @@ public class TreeBacker
 {
     public TreeNode getTreeData()
     {
-        TreeNode treeData = new TreeNodeBase("foo-folder", "Inbox", true);
+        TreeNode treeData = new TreeNodeBase("foo-folder", "Inbox", false);
 
         // construct a set of fake data (normally your data would come from a database)
 
         // populate Frank's portion of the tree
-        TreeNodeBase personNode = new TreeNodeBase("person", "Frank Foo", true);
-        personNode.getChildren().add(new TreeNodeBase("foo-folder", "Requires Foo", true));
-        TreeNodeBase folderNode = new TreeNodeBase("foo-folder", "Requires Foo Reviewer", true);
-        folderNode.getChildren().add(new TreeNodeBase("document", "X050001", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "X050002", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "X050003", false));
+        TreeNodeBase personNode = new TreeNodeBase("person", "Frank Foo", false);
+        personNode.getChildren().add(new TreeNodeBase("foo-folder", "Requires Foo", false));
+        TreeNodeBase folderNode = new TreeNodeBase("foo-folder", "Requires Foo Reviewer", false);
+        folderNode.getChildren().add(new TreeNodeBase("document", "X050001", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "X050002", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "X050003", true));
         personNode.getChildren().add(folderNode);
-        personNode.getChildren().add(new TreeNodeBase("foo-folder", "Requires Foo Recommendation", true));
-        folderNode = new TreeNodeBase("foo-folder", "Requires Foo Approval", true);
-        folderNode.getChildren().add(new TreeNodeBase("document", "J050001", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "J050002", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "J050003", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "E050011", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "R050002", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "C050003", false));
+        personNode.getChildren().add(new TreeNodeBase("foo-folder", "Requires Foo Recommendation", false));
+        folderNode = new TreeNodeBase("foo-folder", "Requires Foo Approval", false);
+        folderNode.getChildren().add(new TreeNodeBase("document", "J050001", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "J050002", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "J050003", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "E050011", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "R050002", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "C050003", true));
         personNode.getChildren().add(folderNode);
-        folderNode = new TreeNodeBase("bar-folder", "Requires Bar Processing", true);
-        folderNode.getChildren().add(new TreeNodeBase("document", "X050003", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "X050011", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "F050002", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "G050003", false));
+        folderNode = new TreeNodeBase("bar-folder", "Requires Bar Processing", false);
+        folderNode.getChildren().add(new TreeNodeBase("document", "X050003", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "X050011", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "F050002", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "G050003", true));
         personNode.getChildren().add(folderNode);
-        folderNode = new TreeNodeBase("bar-folder", "Requires Bar Approval", true);
-        folderNode.getChildren().add(new TreeNodeBase("document", "J050006", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "J050007", false));
+        folderNode = new TreeNodeBase("bar-folder", "Requires Bar Approval", false);
+        folderNode.getChildren().add(new TreeNodeBase("document", "J050006", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "J050007", true));
         personNode.getChildren().add(folderNode);
 
         treeData.getChildren().add(personNode);
 
         // populate Betty's portion of the tree
-        personNode = new TreeNodeBase("person", "Betty Bar", true);
-        personNode.getChildren().add(new TreeNodeBase("foo-folder", "Requires Foo", true));
-        folderNode = new TreeNodeBase("foo-folder", "Requires Foo Reviewer", true);
-        folderNode.getChildren().add(new TreeNodeBase("document", "X012000", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "X013000", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "X014000", false));
+        personNode = new TreeNodeBase("person", "Betty Bar", false);
+        personNode.getChildren().add(new TreeNodeBase("foo-folder", "Requires Foo", false));
+        folderNode = new TreeNodeBase("foo-folder", "Requires Foo Reviewer", false);
+        folderNode.getChildren().add(new TreeNodeBase("document", "X012000", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "X013000", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "X014000", true));
         personNode.getChildren().add(folderNode);
-        folderNode = new TreeNodeBase("foo-folder", "Requires Foo Recommendation", true);
-        folderNode.getChildren().add(new TreeNodeBase("document", "J010026", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "J020002", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "J030103", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "E030214", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "R020444", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "C010000", false));
+        folderNode = new TreeNodeBase("foo-folder", "Requires Foo Recommendation", false);
+        folderNode.getChildren().add(new TreeNodeBase("document", "J010026", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "J020002", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "J030103", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "E030214", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "R020444", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "C010000", true));
         personNode.getChildren().add(folderNode);
-        personNode.getChildren().add(new TreeNodeBase("foo-folder", "Requires Foo Approval", true));
-        folderNode = new TreeNodeBase("bar-folder", "Requires Bar Processing", true);
-        folderNode.getChildren().add(new TreeNodeBase("document", "T052003", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "T020011", false));
+        personNode.getChildren().add(new TreeNodeBase("foo-folder", "Requires Foo Approval", false));
+        folderNode = new TreeNodeBase("bar-folder", "Requires Bar Processing", false);
+        folderNode.getChildren().add(new TreeNodeBase("document", "T052003", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "T020011", true));
         personNode.getChildren().add(folderNode);
-        folderNode = new TreeNodeBase("bar-folder", "Requires Bar Approval", true);
-        folderNode.getChildren().add(new TreeNodeBase("document", "J010002", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "J030047", false));
-        folderNode.getChildren().add(new TreeNodeBase("document", "F030112", false));
+        folderNode = new TreeNodeBase("bar-folder", "Requires Bar Approval", false);
+        folderNode.getChildren().add(new TreeNodeBase("document", "J010002", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "J030047", true));
+        folderNode.getChildren().add(new TreeNodeBase("document", "F030112", true));
         personNode.getChildren().add(folderNode);
 
         treeData.getChildren().add(personNode);
 
         return treeData;
-    }
-
-    public TreeNode getSimpleTreeData()
-    {
-        TreeNode treeData = new TreeNodeBase("branch", "Inbox", true);
-
-        // construct a set of fake data (normally your data would come from a database)
-
-        // populate Frank's portion of the tree
-        TreeNodeBase branchNode = new TreeNodeBase("branch", "Frank Foo", true);
-        branchNode.getChildren().add(new TreeNodeBase("branch", "Requires Foo", true));
-        TreeNodeBase folderNode = new TreeNodeBase("branch", "Requires Foo Reviewer", true);
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "X050001", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "X050002", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "X050003", false));
-        branchNode.getChildren().add(folderNode);
-        branchNode.getChildren().add(new TreeNodeBase("branch", "Requires Foo Recommendation", true));
-        folderNode = new TreeNodeBase("branch", "Requires Foo Approval", true);
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "J050001", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "J050002", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "J050003", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "E050011", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "R050002", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "C050003", false));
-        branchNode.getChildren().add(folderNode);
-        folderNode = new TreeNodeBase("branch", "Requires Bar Processing", true);
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "X050003", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "X050011", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "F050002", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "G050003", false));
-        branchNode.getChildren().add(folderNode);
-        folderNode = new TreeNodeBase("branch", "Requires Bar Approval", true);
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "J050006", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "J050007", false));
-        branchNode.getChildren().add(folderNode);
-
-        treeData.getChildren().add(branchNode);
-
-        // populate Betty's portion of the tree
-        branchNode = new TreeNodeBase("branch", "Betty Bar", true);
-        branchNode.getChildren().add(new TreeNodeBase("branch", "Requires Foo", true));
-        folderNode = new TreeNodeBase("branch", "Requires Foo Reviewer", true);
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "X012000", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "X013000", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "X014000", false));
-        branchNode.getChildren().add(folderNode);
-        folderNode = new TreeNodeBase("branch", "Requires Foo Recommendation", true);
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "J010026", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "J020002", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "J030103", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "E030214", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "R020444", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "C010000", false));
-        branchNode.getChildren().add(folderNode);
-        branchNode.getChildren().add(new TreeNodeBase("branch", "Requires Foo Approval", true));
-        folderNode = new TreeNodeBase("branch", "Requires Bar Processing", true);
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "T052003", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "T020011", false));
-        branchNode.getChildren().add(folderNode);
-        folderNode = new TreeNodeBase("branch", "Requires Bar Approval", true);
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "J010002", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "J030047", false));
-        folderNode.getChildren().add(new TreeNodeBase("leaf", "F030112", false));
-        branchNode.getChildren().add(folderNode);
-
-        treeData.getChildren().add(branchNode);
-
-        return treeData;
-
     }
 }
