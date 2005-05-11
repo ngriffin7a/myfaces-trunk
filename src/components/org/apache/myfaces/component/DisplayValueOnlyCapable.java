e91f66114057f57f40c594be1cee6541a928f794
Martin Marinschek <mmarinschek@apache.org>
2005-05-11 15:13:20 +0000

    new feature - displayValueOnlyCapable components.
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@169643 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/components/org/apache/myfaces/component/DisplayValueOnlyCapable.java
@@ -0,0 +1,14 @@
+package org.apache.myfaces.component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: developer
+ * Date: 11.05.2005
+ * Time: 17:08:05
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DisplayValueOnlyCapable
+{
+    public boolean isDisplayValueOnly();
+    public void setDisplayValueOnly(boolean displayValueOnly);
+}
