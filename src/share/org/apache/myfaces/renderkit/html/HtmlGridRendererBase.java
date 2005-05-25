cc9e9193267cfecd90fe0ff2796aa364749d395a
Martin Marinschek <mmarinschek@apache.org>
2005-05-25 14:53:43 +0000

    Thanks to Mathias Broekelmann for his suggestion
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@178447 13f79535-47bb-0310-9956-ffa450edef68

--- src/share/org/apache/myfaces/renderkit/html/HtmlGridRendererBase.java
+++ src/share/org/apache/myfaces/renderkit/html/HtmlGridRendererBase.java
@@ -139,111 +139,99 @@ public class HtmlGridRendererBase
                                 int columns)
         throws IOException
     {
-        try
+        writer.startElement(HTML.TBODY_ELEM, component);
+
+        String columnClasses;
+        String rowClasses;
+        if (component instanceof HtmlPanelGrid)
         {
-            writer.startElement(HTML.TBODY_ELEM, component);
+            columnClasses = ((HtmlPanelGrid)component).getColumnClasses();
+            rowClasses =  ((HtmlPanelGrid)component).getRowClasses();
+        }
+        else
+        {
+            columnClasses = (String)component.getAttributes().get(JSFAttr.COLUMN_CLASSES_ATTR);
+            rowClasses = (String)component.getAttributes().get(JSFAttr.ROW_CLASSES_ATTR);
+        }
 
-            String columnClasses;
-            String rowClasses;
-            if (component instanceof HtmlPanelGrid)
+        String[] columnClassesArray = (columnClasses == null)
+            ? ArrayUtils.EMPTY_STRING_ARRAY
+            : StringUtils.trim(StringUtils.splitShortString(columnClasses, ','));
+        int columnClassesCount = columnClassesArray.length;
+
+        String[] rowClassesArray = (rowClasses == null)
+            ? ArrayUtils.EMPTY_STRING_ARRAY
+            : StringUtils.trim(StringUtils.splitShortString(rowClasses, ','));
+        int rowClassesCount = rowClassesArray.length;
+
+        int childCount = component.getChildCount();
+        if (childCount > 0)
+        {
+            int columnIndex = 0;
+            int rowClassIndex = 0;
+            boolean rowStarted = false;
+            for (Iterator it = component.getChildren().iterator(); it.hasNext(); )
             {
-                columnClasses = ((HtmlPanelGrid)component).getColumnClasses();
-                rowClasses =  ((HtmlPanelGrid)component).getRowClasses();
-            }
-            else
-            {
-                columnClasses = (String)component.getAttributes().get(JSFAttr.COLUMN_CLASSES_ATTR);
-                rowClasses = (String)component.getAttributes().get(JSFAttr.ROW_CLASSES_ATTR);
-            }
-
-            String[] columnClassesArray = (columnClasses == null)
-                ? ArrayUtils.EMPTY_STRING_ARRAY
-                : StringUtils.trim(StringUtils.splitShortString(columnClasses, ','));
-            int columnClassesCount = columnClassesArray.length;
-
-            String[] rowClassesArray = (rowClasses == null)
-                ? ArrayUtils.EMPTY_STRING_ARRAY
-                : StringUtils.trim(StringUtils.splitShortString(rowClasses, ','));
-            int rowClassesCount = rowClassesArray.length;
-
-            int childCount = component.getChildCount();
-            if (childCount > 0)
-            {
-                int columnIndex = 0;
-                int rowClassIndex = 0;
-                boolean rowStarted = false;
-                for (Iterator it = component.getChildren().iterator(); it.hasNext(); )
+                UIComponent child = (UIComponent)it.next();
+                if (child.isRendered())
                 {
-                    UIComponent child = (UIComponent)it.next();
-                    if (child.isRendered())
+                    if (columnIndex == 0)
                     {
-                        if (columnIndex == 0)
+                        //start of new/next row
+                        if (rowStarted)
                         {
-                            //start of new/next row
-                            if (rowStarted)
-                            {
-                                //do we have to close the last row?
-                                writer.endElement(HTML.TR_ELEM);
-                                HtmlRendererUtils.writePrettyLineSeparator(context);
-                            }
-                            writer.startElement(HTML.TR_ELEM, component);
-                            if (rowClassIndex < rowClassesCount) {
-                                writer.writeAttribute(HTML.CLASS_ATTR, rowClassesArray[rowClassIndex], null);
-                            }
-                            rowStarted = true;
-                            rowClassIndex++;
-                            if (rowClassIndex == rowClassesCount) {
-                                rowClassIndex = 0;
-                            }
+                            //do we have to close the last row?
+                            writer.endElement(HTML.TR_ELEM);
+                            HtmlRendererUtils.writePrettyLineSeparator(context);
                         }
+                        writer.startElement(HTML.TR_ELEM, component);
+                        if (rowClassIndex < rowClassesCount) {
+                            writer.writeAttribute(HTML.CLASS_ATTR, rowClassesArray[rowClassIndex], null);
+                        }
+                        rowStarted = true;
+                        rowClassIndex++;
+                        if (rowClassIndex == rowClassesCount) {
+                            rowClassIndex = 0;
+                        }
+                    }
 
+                    writer.startElement(HTML.TD_ELEM, component);
+                    if (columnIndex < columnClassesCount)
+                    {
+                        writer.writeAttribute(HTML.CLASS_ATTR, columnClassesArray[columnIndex], null);
+                    }
+                    RendererUtils.renderChild(context, child);
+                    writer.endElement(HTML.TD_ELEM);
+
+                    columnIndex++;
+                    if (columnIndex >= columns) {
+                        columnIndex = 0;
+                    }
+                }
+            }
+
+            if (rowStarted)
+            {
+                if (columnIndex > 0)
+                {
+                    if (log.isWarnEnabled()) log.warn("PanelGrid " + component.getClientId(context) + " has not enough children. Child count should be a multiple of the columns attribute.");
+                    //Render empty columns, so that table is correct
+                    for ( ; columnIndex < columns; columnIndex++)
+                    {
                         writer.startElement(HTML.TD_ELEM, component);
                         if (columnIndex < columnClassesCount)
                         {
                             writer.writeAttribute(HTML.CLASS_ATTR, columnClassesArray[columnIndex], null);
                         }
-                        RendererUtils.renderChild(context, child);
                         writer.endElement(HTML.TD_ELEM);
-
-                        columnIndex++;
-                        if (columnIndex >= columns) {
-                            columnIndex = 0;
-                        }
                     }
                 }
-
-                if (rowStarted)
-                {
-                    if (columnIndex > 0)
-                    {
-                        if (log.isWarnEnabled()) log.warn("PanelGrid " + component.getClientId(context) + " has not enough children. Child count should be a multiple of the columns attribute.");
-                        //Render empty columns, so that table is correct
-                        for ( ; columnIndex < columns; columnIndex++)
-                        {
-                            writer.startElement(HTML.TD_ELEM, component);
-                            if (columnIndex < columnClassesCount)
-                            {
-                                writer.writeAttribute(HTML.CLASS_ATTR, columnClassesArray[columnIndex], null);
-                            }
-                            writer.endElement(HTML.TD_ELEM);
-                        }
-                    }
-                    writer.endElement(HTML.TR_ELEM);
-                    HtmlRendererUtils.writePrettyLineSeparator(context);
-                }
+                writer.endElement(HTML.TR_ELEM);
+                HtmlRendererUtils.writePrettyLineSeparator(context);
             }
+        }
 
-            writer.endElement(HTML.TBODY_ELEM);
-        }
-        catch(IOException ex)
-        {
-            throw ex;
-        }
-        catch(RuntimeException ex2)
-        {
-            log.error("Exception while rendering children of panel-grid.",ex2);
-            throw ex2;
-        }
+        writer.endElement(HTML.TBODY_ELEM);
     }
 
 }
