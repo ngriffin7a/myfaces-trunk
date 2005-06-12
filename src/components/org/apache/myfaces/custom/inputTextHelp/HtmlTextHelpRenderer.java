b1cd3563ae71ec45dec6fed28a9efc898bd030b2
Martin Marinschek <mmarinschek@apache.org>
2005-06-12 12:41:16 +0000

    git-svn-id: https://svn.apache.org/repos/asf/myfaces/trunk@190263 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/components/org/apache/myfaces/custom/inputTextHelp/HtmlTextHelpRenderer.java
@@ -0,0 +1,124 @@
+package org.apache.myfaces.custom.inputTextHelp;
+
+import org.apache.myfaces.renderkit.html.HtmlTextRendererBase;
+import org.apache.myfaces.renderkit.html.HtmlRendererUtils;
+import org.apache.myfaces.renderkit.html.HTML;
+import org.apache.myfaces.renderkit.html.ext.HtmlTextRenderer;
+import org.apache.myfaces.renderkit.html.util.HTMLEncoder;
+import org.apache.myfaces.renderkit.RendererUtils;
+import org.apache.myfaces.component.html.util.AddResource;
+
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+
+import java.io.IOException;
+
+/**
+ * @author Thomas Obereder
+ * @version Date: 09.06.2005, 22:50:48
+ */
+public class HtmlTextHelpRenderer extends HtmlTextRenderer
+{
+    protected void renderNormal(FacesContext facesContext, UIComponent component) throws IOException
+    {
+        if(component instanceof HtmlInputTextHelp)
+        {
+            HtmlInputTextHelp helpTextComp = (HtmlInputTextHelp) component;
+            addJavaScriptResources(facesContext);
+            renderInputTextHelp(facesContext, (UIInput)helpTextComp);
+        }
+        else
+        {
+            super.renderNormal(facesContext, component);
+        }
+    }
+
+    public static boolean isSelectText(UIComponent component)
+    {
+        if(component instanceof HtmlInputTextHelp)
+        {
+            HtmlInputTextHelp helpTextComp = (HtmlInputTextHelp) component;
+            return helpTextComp.isSelectText();
+        }
+        return false;
+    }
+
+    public static String getHelpText(UIComponent component)
+    {
+        if(component instanceof HtmlInputTextHelp)
+        {
+            HtmlInputTextHelp helpTextComp = (HtmlInputTextHelp) component;
+            if(helpTextComp.getHelpText() != null)
+                return helpTextComp.getHelpText();
+        }
+        return null;
+    }
+
+    public static void renderInputTextHelp(FacesContext facesContext, UIInput input)
+            throws IOException
+    {
+        ResponseWriter writer = facesContext.getResponseWriter();
+
+        writer.startElement(HTML.INPUT_ELEM, input);
+
+        HtmlRendererUtils.writeIdIfNecessary(writer, input, facesContext);
+
+        renderHelpTextAttributes(input, writer, facesContext);
+
+        String value = RendererUtils.getStringValue(facesContext, input);
+        value = (value.equals("") || value == null) ? getHelpText(input) : "";
+
+        writer.writeAttribute(HTML.VALUE_ATTR, HTMLEncoder.encode(value,true,true), null);
+
+        writer.endElement(HTML.INPUT_ELEM);
+    }
+
+    public static void renderHelpTextAttributes(UIComponent component,
+                                                ResponseWriter writer,
+                                                FacesContext facesContext)
+            throws IOException
+    {
+        if(!(component instanceof HtmlInputTextHelp))
+        {
+            HtmlRendererUtils.renderHTMLAttributes(writer, component, HTML.COMMON_PASSTROUGH_ATTRIBUTES);
+            return;
+        }
+        else
+        {
+            String id = component.getClientId(facesContext);
+            if(isSelectText(component))
+            {
+                HtmlRendererUtils.renderHTMLAttributes(writer, component,
+                        HTML.COMMON_PASSTROUGH_ATTRIBUTES_WITHOUT_ONCLICK);
+                writer.writeAttribute(HTML.ONCLICK_ATTR,
+                        HtmlInputTextHelp.JS_FUNCTION_SELECT_TEXT + "('" +
+                            getHelpText(component) + "', '" + id +"')", null);
+            }
+            else
+            {
+                if(getHelpText(component) != null)
+                {
+                    HtmlRendererUtils.renderHTMLAttributes(writer, component,
+                            HTML.COMMON_PASSTROUGH_ATTRIBUTES_WITHOUT_ONCLICK);
+                    writer.writeAttribute(HTML.ONCLICK_ATTR,
+                            HtmlInputTextHelp.JS_FUNCTION_RESET_HELP + "('" +
+                            getHelpText(component) + "', '" + id +"')", null);
+                }
+                else
+                {
+                    HtmlRendererUtils.renderHTMLAttributes(writer,
+                            component, HTML.COMMON_PASSTROUGH_ATTRIBUTES);
+                }
+            }
+        }
+    }
+
+    private static void addJavaScriptResources(FacesContext facesContext) throws IOException
+    {
+        AddResource.addJavaScriptToHeader(HtmlTextHelpRenderer.class,
+                                            "inputTextHelp.js",
+                                            facesContext);
+    }
+}
