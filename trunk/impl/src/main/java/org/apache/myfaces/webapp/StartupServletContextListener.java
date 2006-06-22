ad29e874b3b5ed1a79f35d5ccf4b0528dfbf9ca7
Matthias Wessendorf <matzew@apache.org>
2006-06-22 02:20:55 +0000

    made a copy
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/core/branches/1_1_4@416219 13f79535-47bb-0310-9956-ffa450edef68

---
+++ trunk/impl/src/main/java/org/apache/myfaces/webapp/StartupServletContextListener.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.myfaces.webapp;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javax.faces.FactoryFinder;
+import javax.faces.context.ExternalContext;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.myfaces.config.FacesConfigValidator;
+import org.apache.myfaces.config.FacesConfigurator;
+import org.apache.myfaces.context.servlet.ServletExternalContextImpl;
+import org.apache.myfaces.shared_impl.util.ClassUtils;
+import org.apache.myfaces.shared_impl.util.StateUtils;
+import org.apache.myfaces.shared_impl.util.serial.DefaultSerialFactory;
+import org.apache.myfaces.shared_impl.util.serial.SerialFactory;
+import org.apache.myfaces.shared_impl.webapp.webxml.WebXml;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author Manfred Geiler (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+public class StartupServletContextListener
+        implements ServletContextListener
+{
+    private static final Log log = LogFactory.getLog(StartupServletContextListener.class);
+
+    static final String FACES_INIT_DONE
+            = StartupServletContextListener.class.getName() + ".FACES_INIT_DONE";
+
+    public void contextInitialized(ServletContextEvent event)
+    {
+        initFaces(event.getServletContext());
+    }
+
+    public static void initFaces(ServletContext servletContext)
+    {
+        try
+        {
+            Boolean b = (Boolean)servletContext.getAttribute(FACES_INIT_DONE);
+
+            if (b == null || b.booleanValue() == false)
+            {
+                log.trace("Initializing MyFaces");
+
+                //Load the configuration
+                ExternalContext externalContext = new ServletExternalContextImpl(servletContext, null, null);
+
+                //And configure everything
+                new FacesConfigurator(externalContext).configure();
+
+                if ("true".equals(servletContext
+                                .getInitParameter(FacesConfigValidator.VALIDATE_CONTEXT_PARAM)))
+                {
+                    List list = FacesConfigValidator.validate(externalContext,
+                            servletContext.getRealPath("/"));
+
+                    Iterator iterator = list.iterator();
+
+                    while (iterator.hasNext())
+                        log.warn(iterator.next());
+
+                }
+                
+                // parse web.xml
+                WebXml.init(externalContext);
+
+                servletContext.setAttribute(FACES_INIT_DONE, Boolean.TRUE);
+            }
+            else
+            {
+                log.info("MyFaces already initialized");
+            }
+        }
+        catch (Exception ex)
+        {
+            log.error("Error initializing ServletContext", ex);
+            ex.printStackTrace();
+        }
+        log.info("ServletContext '" + servletContext.getRealPath("/") + "' initialized.");
+        
+        if(servletContext.getInitParameter(StateUtils.INIT_SECRET) != null)
+            StateUtils.initSecret(servletContext);
+        
+        handleSerialFactory(servletContext);
+    }
+
+    private static void handleSerialFactory(ServletContext servletContext){
+        
+        String serialProvider = servletContext.getInitParameter(StateUtils.SERIAL_FACTORY);
+        SerialFactory serialFactory = null;
+        
+        if(serialProvider == null)
+        {
+            serialFactory = new DefaultSerialFactory();
+        }
+        else
+        {
+            try
+            {
+                serialFactory = (SerialFactory) ClassUtils.newInstance(serialProvider);
+                
+            }catch(ClassCastException e){
+                log.error("Make sure '" + serialProvider + 
+                        "' implements the correct interface", e);
+            }
+            catch(Exception e){
+                log.error(e);
+            }
+            finally
+            {
+                if(serialFactory == null)
+                {
+                    serialFactory = new DefaultSerialFactory();
+                    log.error("Using default serialization provider");
+                }
+            }
+            
+        }
+        
+        log.info("Serialization provider : " + serialFactory.getClass());
+        servletContext.setAttribute(StateUtils.SERIAL_FACTORY, serialFactory);
+        
+    }
+    
+    public void contextDestroyed(ServletContextEvent e)
+    {
+        FactoryFinder.releaseFactories();
+    }
+}
